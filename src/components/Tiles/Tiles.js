ceb2441fa305f776d5a84507e7a66a1f5198a84f
محمد دربندی <darbandi.craft@gmail.com>
2021-05-03 22:49:19 +0430

    add key to map in tiles

--- src/components/Tiles/Tiles.js
+++ src/components/Tiles/Tiles.js
@@ -1,4 +1,3 @@
-import "./Tiles.scss";
 import React from "react";
 import Tile from "./tile/Tile";
 import translate from "./../../assets/img/translate.svg";
@@ -6,36 +5,53 @@ import play from "./../../assets/img/play.svg";
 import listDark from "./../../assets/img/list-dark.svg";
 import Interpretation from "./../../assets/img/Interpretation.svg";
 import virtue from "./../../assets/img/virtue.svg";
+import "./Tiles.scss";
 
 const Tiles = (props) => {
   const { id } = props;
   const tileItems = [
     {
+      id: 1,
       title: "متن و ترجمه آیات",
       url: translate,
       col: "col-12",
       href: `/verses/${id}`,
     },
-    { title: "قرائت", url: play, col: "col-6", href: `/verses/${id}` },
     {
+      id: 2,
+      title: "قرائت",
+      url: play,
+      col: "col-6",
+      href: `/verses/${id}`,
+    },
+    {
+      id: 3,
       title: "لیست کلمات",
       url: listDark,
       col: "col-6",
       href: `/surah-new-words/${id}`,
     },
     {
+      id: 4,
       title: "تفسیر",
       url: Interpretation,
       col: "col-6",
       href: `/verses/${id}`,
     },
-    { title: "فضیلت", url: virtue, col: "col-6", href: `/verses/${id}` },
+    {
+      id: 5,
+      title: "فضیلت",
+      url: virtue,
+      col: "col-6",
+      href: `/verses/${id}`,
+    },
   ];
   return (
     <div className="tiles">
-      {tileItems?.map((item, index) => {
+      {tileItems?.map((item) => {
         return (
           <Tile
+            key={item.id}
             title={item.title}
             url={item.url}
             col={item.col}
