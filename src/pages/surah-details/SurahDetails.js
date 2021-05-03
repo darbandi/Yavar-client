64063306892b63558fbc3c7c07ca87f4a44e1e8a
محمد دربندی <darbandi.craft@gmail.com>
2021-05-03 22:52:42 +0430

    remove comments

--- src/pages/surah-details/SurahDetails.js
+++ src/pages/surah-details/SurahDetails.js
@@ -4,7 +4,6 @@ import Tiles from "../../components/tiles/Tiles";
 import SurahDetailsCard from "../../components/surah-details-card/SurahDetailsCard";
 import { isEmpty } from "lodash";
 import useVerses from "../../api/useVerses";
-import Layout from "../../components/layout/Layout";
 import Loading from "../../components/loading/Loading";
 import Error from "../../components/error/Error";
 
@@ -12,29 +11,21 @@ const SurahDetails = (props) => {
   const { id } = props.match.params;
   const [{ surahData, data, loading, error }] = useVerses(id);
 
-  const layoutOption = {
-    header: {
-      action: ["search", "back"],
-    },
-  };
-
   if (error) return <Error />;
   if (loading) return <Loading />;
 
   return (
-    // <Layout layoutOption={layoutOption}>
-      <div className="surah-details">
-        {!isEmpty(surahData) && (
-          <SurahDetailsCard
-            description={`لیست کلمات سوره ${"ff"}`}
-            icon={"icon-list"}
-            title={""}
-            data={surahData}
-          />
-        )}
-        <Tiles id={id} />
-      </div>
-    // </Layout>
+    <div className="surah-details">
+      {!isEmpty(surahData) && (
+        <SurahDetailsCard
+          description={`لیست کلمات سوره ${"ff"}`}
+          icon={"icon-list"}
+          title={""}
+          data={surahData}
+        />
+      )}
+      <Tiles id={id} />
+    </div>
   );
 };
 
--- src/pages/surah-new-words/SurahNewWords.js
+++ src/pages/surah-new-words/SurahNewWords.js
@@ -1,19 +1,12 @@
 import "./SurahNewWords.scss";
 import React from "react";
-import Layout from "../../components/layout/Layout";
 import SurahDetailsCard from "../../components/surah-details-card/SurahDetailsCard";
 import useVerses from "../../api/useVerses";
 import { isEmpty } from "lodash";
 import Error from "../../components/error/Error";
 import Loading from "../../components/loading/Loading";
 import useSurahNewWords from "../../api/useSurahNewWords";
-import VerseList from "../../components/verse-list/VerseList";
 
-const layoutOption = {
-  header: {
-    action: ["search", "back"],
-  },
-};
 const SurahNewWords = (props) => {
   const { id } = props.match.params;
   const [{ surahData, loading, error }] = useVerses(id);
@@ -25,12 +18,9 @@ const SurahNewWords = (props) => {
   if (loading) return <Loading />;
 
   return (
-    // <Layout layoutOption={layoutOption}>
-      <div className="surah-new-words">
-        {!isEmpty(data) && <SurahDetailsCard data={surahData} />}
-        
-      </div>
-    // </Layout>
+    <div className="surah-new-words">
+      {!isEmpty(data) && <SurahDetailsCard data={surahData} />}
+    </div>
   );
 };
 
